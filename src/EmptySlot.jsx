734a51ac66b810cb337140f845d3f76187316bba
ASAPLito <micbraner@gmail.com>
2022-07-14 16:57:23 -0400

    Initial commit

---
+++ src/EmptySlot.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const EmptySlot = () => {
+    return (
+        <div>
+            <h1>Empty Slot</h1>
+        </div>
+    )
+}
+
+export default EmptySlot;
---
+++ src/MoleContainer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Mole from "./Mole";
+import EmptySlot from "./EmptySlot";
+
+const MoleContainer = (props) => {
+
+
+    return (
+        <div>
+            <h1>Mole Container</h1>
+        </div>
+    )
+}
+
+export default MoleContainer;
